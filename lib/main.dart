c059c35d725756a29342fdb4424a2a59ef9c4a8a
Lans12345 <olanalans12345@gmail.com>
2024-03-12 07:44:24 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:medi_connect/screens/landing_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          authDomain: 'mediconnect-a4edd.firebaseapp.com',
+          apiKey: "AIzaSyBR2B-YBYaEwJ0WiKVCU_0vPkw4DpmqAfs",
+          appId: "1:701825559115:web:8e152e9d12b54162e2bbce",
+          messagingSenderId: "701825559115",
+          projectId: "mediconnect-a4edd",
+          storageBucket: "mediconnect-a4edd.appspot.com"));
   runApp(const MyApp());
 }
 
